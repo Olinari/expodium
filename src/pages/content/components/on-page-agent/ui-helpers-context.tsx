1788e3d55029b49ca45efa6d402c9255fdc849cb
ariel.olin@apiiro.com <114854641+arielolin@users.noreply.github.com>
2023-10-18 20:04:50 +0300

    fix

--- src/pages/content/components/on-page-agent/ui-helpers-context.tsx
+++ src/pages/content/components/on-page-agent/ui-helpers-context.tsx
@@ -1,4 +1,11 @@
-import React, { createContext, useContext, useState, ReactNode } from "react";
+import React, {
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+  useCallback,
+  useEffect,
+} from "react";
 import _ from "lodash";
 import { getMostOverlappedElement } from "@src/utils/webpage-utils";
 
@@ -20,62 +27,89 @@ interface UiHelpersProviderProps {
 
 export const UiHelpersProvider = ({ children }: UiHelpersProviderProps) => {
   const [elementsData, setElements] = useState([]);
-  const [selectedElementIndex, setSelctedElementIndex] = useState(0);
+  const [selectedElementIndex, setSelctedElementIndex] = useState([0, 0]);
 
   const updateUiHelperControls = (data: any) => {
-    setElements(
-      reduce(
-        data
-          .sort((a, b) => {
-            return a.y - b.y; // sort by x first (left to right)
-          })
-          .sort((a, b) => a.x - b.x)
-      )
-    );
+    setElements(sortIntoMatrix(removeRedundatRects(data)));
   };
 
   const offset = 8 as const;
 
+  const navigate = useCallback(
+    (event) => {
+      let [row, col] = selectedElementIndex;
+
+      switch (event.key) {
+        case "a": // Left
+          col = Math.max(0, col - 1);
+          break;
+        case "s": // Down
+          row = Math.min(elementsData.length - 1, row + 1);
+          col = Math.min(elementsData[row].length - 1, col); // adjust column if the next row has fewer items
+          break;
+        case "d": // Right
+          col = Math.min(elementsData[row].length - 1, col + 1);
+          break;
+        case "w": // Up
+          row = Math.max(0, row - 1);
+          col = Math.min(elementsData[row].length - 1, col); // adjust column if the previous row has fewer items
+          break;
+        default:
+          return; // If other keys are pressed, don't do anything
+      }
+
+      setSelctedElementIndex([row, col]);
+    },
+    [selectedElementIndex, elementsData]
+  );
+
+  useEffect(() => {
+    document.addEventListener("keydown", navigate);
+    return () => document.removeEventListener("keydown", navigate);
+  }, [navigate]);
+
   return (
     <UiHelpersContext.Provider value={{ updateUiHelperControls }}>
       <>
-        {elementsData.map((element, index) => {
-          console.log(selectedElementIndex, index);
-          const width = offset + element.width;
-          const height = offset + element.height;
-          const top = element.y - 0.5 * element.height - offset / 2;
-          const left = element.x - 0.5 * element.width - offset / 2;
-          return (
-            <div
-              onClick={() =>
-                console.log(
-                  getMostOverlappedElement(
-                    left,
-                    top,
-                    width,
-                    height,
-                    element.tag
+        {elementsData.map((row, y) => {
+          return row.map((element, x) => {
+            const width = offset + element.width;
+            const height = offset + element.height;
+            const top = element.y - 0.5 * element.height - offset / 2;
+            const left = element.x - 0.5 * element.width - offset / 2;
+            return (
+              <div
+                onClick={() =>
+                  console.log(
+                    getMostOverlappedElement(
+                      left,
+                      top,
+                      width,
+                      height,
+                      element.tag
+                    )
                   )
-                )
-              }
-              key={index}
-              style={{
-                border:
-                  selectedElementIndex === index
-                    ? "2px solid blue"
-                    : "2px solid red",
-                position: "absolute",
-                top: `${top}px`,
-                left: `${left}px`,
-                width: `${width}px`,
-                height: `${height}px`,
-                zIndex: 10000,
-              }}
-              data-slicerra={true}
-              data-index={element.id}
-              data-element={element.class}
-            />
-          );
+                }
+                key={`${x}-${y}`}
+                style={{
+                  border:
+                    selectedElementIndex[0] === y &&
+                    selectedElementIndex[1] === x
+                      ? "2px solid blue"
+                      : "2px solid transparentw",
+                  position: "absolute",
+                  top: `${top}px`,
+                  left: `${left}px`,
+                  width: `${width}px`,
+                  height: `${height}px`,
+                  zIndex: 10000,
+                }}
+                data-slicerra={true}
+                data-index={element.id}
+                data-element={element.class}
+              />
+            );
+          });
         })}
       </>
       {children}
@@ -109,7 +143,7 @@ function mergeRectangles(a, b) {
   };
 }
 
-const reduce = (data) => {
+const removeRedundatRects = (data) => {
   const reducedData = [...data];
   for (let i = 0; i < reducedData.length; i++) {
     reducedData[i].index = _.uniqueId;
@@ -125,3 +159,30 @@ const reduce = (data) => {
 
   return reducedData;
 };
+
+function sortIntoMatrix(points, deltaY = 40) {
+  // Sort primarily by y, then by x
+  points.sort((a, b) => a.y - b.y || a.x - b.x);
+
+  const rows = [];
+  let currentRow = [points[0]];
+
+  for (let i = 1; i < points.length; i++) {
+    if (Math.abs(points[i].y - points[i - 1].y) <= deltaY) {
+      currentRow.push(points[i]);
+    } else {
+      // Sort the current row by x-values before pushing to the rows array
+      currentRow.sort((a, b) => a.x - b.x);
+      rows.push(currentRow);
+      currentRow = [points[i]];
+    }
+  }
+
+  // Don't forget the last row
+  if (currentRow.length) {
+    currentRow.sort((a, b) => a.x - b.x);
+    rows.push(currentRow);
+  }
+
+  return rows;
+}
